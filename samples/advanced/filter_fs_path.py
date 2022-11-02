564a853d5e1d8844f0d110691021c2184c11ba76
Igor Rogov <igor.rogov@vound-software.com>
2022-11-02 17:05:28 +1100

    added a script for filtering by fs path

---
+++ samples/advanced/filter_fs_path.py
@@ -0,0 +1,29 @@
+from api.scripting import ScriptService
+from api.scripting.ScriptService import Action, FoundItemResult, ProcessedItemResult
+
+
+class ScriptHandler(ScriptService.Iface):
+
+    FOLDERS_TO_SKIP = [
+        "Windows\\Help",
+        "ProgramData\\Microsoft\\Media Player",
+        "Users\\Default"
+        "Users\\Public"
+    ]
+
+    def init(self):
+        # convert paths to lower-case
+        new_set = set()
+        for path in self.FOLDERS_TO_SKIP:
+            new_set.add(path.lower())
+
+        self.FOLDERS_TO_SKIP = new_set
+
+    def itemFound(self, item):
+        if item.fsPath is not None and item.fsPath.lower().strip("\\") in self.FOLDERS_TO_SKIP:
+            return FoundItemResult(action=Action.Skip)
+
+        return FoundItemResult(action=Action.Include)
+
+    def itemProcessed(self, item):
+        return ProcessedItemResult(action=Action.Include)
