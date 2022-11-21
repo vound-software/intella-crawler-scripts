18c2e9d24e5de2daa5106f8e41879ed32408081b
Igor Rogov <igor.rogov@vound-software.com>
2022-11-21 20:44:22 +1100

    added sample file for accessing item text

---
+++ samples/advanced/access_text.py
@@ -0,0 +1,18 @@
+from api.scripting import ScriptService
+from api.scripting.ScriptService import Action, FoundItemResult, ProcessedItemResult
+from pathlib import Path
+
+
+class ScriptHandler(ScriptService.Iface):
+    SKIP_TEXT = "text to exclude"
+
+    def itemFound(self, item):
+        return FoundItemResult(action=Action.Include)
+
+    def itemProcessed(self, item):
+        if item.textFile is not None:
+            text = Path(item.textFile).read_text()
+            if self.SKIP_TEXT in text:
+                return ProcessedItemResult(action=Action.Skip)
+
+        return ProcessedItemResult(action=Action.Include)
