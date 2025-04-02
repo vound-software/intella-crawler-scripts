815f9052155eee77b427aebafa9e8d5ebdb42e1f
Igor Rogov <106656937+IgorRogovVound@users.noreply.github.com>
2025-04-02 16:44:14 +1100

    Update filter_by_type_and_size.py

--- samples/advanced/filter_by_type_and_size.py
+++ samples/advanced/filter_by_type_and_size.py
@@ -19,37 +19,38 @@ def get_item_size(item):
 class ScriptHandler(ScriptService.Iface):
 
     def itemFound(self, item):
+        return FoundItemResult(action=Action.Include)
+
+    def itemProcessed(self, item):
+
         # Any unknown binary file or executable
         if item.mediaType == "application/octet-stream" or item.mediaType == "application/x-ms-dos-executable":
             if item.isTopLevelParent:
-                return FoundItemResult(action=Action.Stub)
+                return ProcessedItemResult(action=Action.Stub)
 
         # MS Access Database (mdb)
         if item.mediaType == "application/vnd.ms-access":
-            return FoundItemResult(action=Action.Stub)
+            return ProcessedItemResult(action=Action.Stub)
 
         size = get_item_size(item)
 
         # Video file larger than 10mb
         if item.mediaType is not None and item.mediaType.startswith("video/") and size > 10 * MB:
-            return FoundItemResult(action=Action.Stub)
+            return ProcessedItemResult(action=Action.Stub)
 
         # PDFs larger than 20mb
         if item.mediaType == "application/pdf" and size > 20 * MB:
-            return FoundItemResult(action=Action.Stub)
+            return ProcessedItemResult(action=Action.Stub)
 
         # Excel spreadsheets larger than 30mb (both XLS and XLSX)
         if item.mediaType == "application/vnd.ms-excel" or item.mediaType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
             if size > 30 * MB:
-                return FoundItemResult(action=Action.Stub)
+                return ProcessedItemResult(action=Action.Stub)
 
         # Small embedded images
         if item.mediaType is not None and item.mediaType.startswith("image/") and size < 10 * KB:
             if not item.isTopLevelParent:
-                return FoundItemResult(action=Action.Stub)
+                return ProcessedItemResult(action=Action.Stub)
 
         # include all other items
-        return FoundItemResult(action=Action.Include)
-
-    def itemProcessed(self, item):
         return ProcessedItemResult(action=Action.Include)
